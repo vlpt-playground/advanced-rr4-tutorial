66ad1f436a4a93b39318a620f5f49da1b1fe63b1
velopert <public.velopert@gmail.com>
2017-06-03 22:26:18 +0900

    1-3

---
+++ src/components/Menu.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Menu = () => {
+    const activeStyle = {
+        color: 'green',
+        fontSize: '2rem'
+    };
+
+    return (
+        <div>
+            <ul>
+                <li><NavLink exact to="/" activeStyle={activeStyle} >Home</NavLink></li>
+                <li><NavLink exact to="/about" activeStyle={activeStyle} >About</NavLink></li>
+                <li><NavLink to="/about/foo" activeStyle={activeStyle} >About Foo</NavLink></li>
+            </ul>
+            <hr/>
+        </div>
+    );
+};
+
+export default Menu;
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Home, About } from 'pages';
+import Menu from 'components/Menu';
 
 class App extends Component {
     render() {
         return (
             <div>
+                <Menu/>
                 <Route exact path="/" component={Home}/>
                 <Switch>
                     <Route path="/about/:name" component={About}/>
