70c39b9d7b79675a6bb07626a4aa7b08c25288d4
velopert <public.velopert@gmail.com>
2017-06-03 22:25:03 +0900

    02

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Home, About } from 'pages';
 
 class App extends Component {
     render() {
         return (
             <div>
-                Hello React-Router
+                <Route exact path="/" component={Home}/>
+                <Switch>
+                    <Route path="/about/:name" component={About}/>
+                    <Route path="/about" component={About}/>
+                </Switch>
             </div>
         );
     }
