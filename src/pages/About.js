6cd5a632389294b2f9f952ad7aefa0b85cae8ca6
velopert <public.velopert@gmail.com>
2017-06-06 22:31:53 +0900

    Helmet Configuration

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import queryString from 'query-string';
+import { Helmet } from 'react-helmet';
 
 const About = ({location, match}) => {
     const query = queryString.parse(location.search);
     
     const detail = query.detail === 'true';
-    
+    const { name } = match.params;
+
     return (
         <div>
-            <h2>About {match.params.name}</h2>
+            <Helmet>
+                <title>{`About ${name ? name : ''}`}</title>
+            </Helmet>
+            <h2>About {name}</h2>
             {detail && 'detail: blahblah'}
         </div>
     );
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Home, About, Posts, Users } from 'pages';
-
+import { Helmet } from "react-helmet";
 import Menu from 'components/Menu';
 
 class App extends Component {
     render() {
         return (
             <div>
+                <Helmet>
+                    <title>React Router & SSR</title>
+                </Helmet>
                 <Menu/>
                 <Route exact path="/" component={Home}/>
                 <Route path="/posts" component={Posts}/>
