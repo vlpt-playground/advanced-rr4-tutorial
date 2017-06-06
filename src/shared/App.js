b0259ac236ec38f4b34e733deebad0080bd0026e
velopert <public.velopert@gmail.com>
2017-06-06 22:30:57 +0900

    added Users Route

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Home, About, Posts, Users } from 'pages';
+
 import Menu from 'components/Menu';
 
 class App extends Component {
